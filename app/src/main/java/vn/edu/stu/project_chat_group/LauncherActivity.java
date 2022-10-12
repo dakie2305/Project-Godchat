edcaa06b8343ffe050c28d69ef126c2aaa200311
Thanh Duc <satthubongmoi2305@gmail.com>
2022-10-12 22:11:59 +0700

    remove action bar
    xoá thanh tiêu đề

--- app/src/main/java/vn/edu/stu/project_chat_group/LauncherActivity.java
+++ app/src/main/java/vn/edu/stu/project_chat_group/LauncherActivity.java
@@ -13,6 +13,7 @@ public class LauncherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_launcher);
 
         progressBar = findViewById(R.id.progressBar);
